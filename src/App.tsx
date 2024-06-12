6152a149081f13e3a1df092088e0d81ab14a25fe
Jerel Miller <jerelmiller@gmail.com>
2024-06-12 15:51:01 -0600

    Setup example with useDeferredValue and promises

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, use, Suspense, useDeferredValue } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+let count = 0;
+const cache = new Map<string, Promise<number>>();
+
+function fetchNextNumber(text: string) {
+  if (cache.has(text)) {
+    return cache.get(text)!;
+  }
+
+  const nextCount = count++;
+  const promise = new Promise<number>((resolve) => {
+    setTimeout(() => {
+      resolve(nextCount);
+    }, 5000);
+  });
+
+  cache.set(text, promise);
+
+  return promise;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("");
+  const deferredText = useDeferredValue(text);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <Suspense fallback={<div>Loading count...</div>}>
+        <Count text={deferredText} />
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+function Count({ text }: { text: string }) {
+  const count = use(fetchNextNumber(text));
+
+  return <div>Number: {count}</div>;
+}
+
+export default App;
