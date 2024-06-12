92c76768db1152a09e43ae57a84b94a506fb8999
Jerel Miller <jerelmiller@gmail.com>
2024-06-12 17:39:59 -0600

    Use userEvent to simulate keyboard event

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState, use, Suspense, useDeferredValue } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, use, useRef, Suspense, useDeferredValue } from "react";
+import { userEvent } from "@testing-library/user-event";
 import "./App.css";
 
 let count = 0;
+const user = userEvent.setup();
 const cache = new Map<string, Promise<number>>();
 
 function fetchNextNumber(text: string) {
@@ -15,7 +15,7 @@ function fetchNextNumber(text: string) {
   const promise = new Promise<number>((resolve) => {
     setTimeout(() => {
       resolve(nextCount);
-    }, 5000);
+    }, 2000);
   });
 
   cache.set(text, promise);
@@ -24,16 +24,22 @@ function fetchNextNumber(text: string) {
 }
 
 function App() {
+  const ref = useRef<HTMLInputElement>(null);
   const [text, setText] = useState("");
   const deferredText = useDeferredValue(text);
+  console.log({ text, deferredText });
 
   return (
     <>
       <input
+        ref={ref}
         type="text"
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
+      <button onClick={async () => await user.type(ref.current!, "ab")}>
+        Update using userEvent
+      </button>
       <Suspense fallback={<div>Loading count...</div>}>
         <Count text={deferredText} />
       </Suspense>
