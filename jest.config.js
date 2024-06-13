5851917ab432291613ef0decb76af715c58d1d9b
Jerel Miller <jerelmiller@gmail.com>
2024-06-13 13:20:10 -0600

    Turn off ts errors in tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,15 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        diagnostics: {
+          warnOnly: true,
+        },
+      },
+    ],
+  },
 };
 
